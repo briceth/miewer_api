e2218be2f0eab279315a2b506d76d89fdfa5e69d
Brice Tessier Huort <tessierhuort@gmail.com>
2016-12-06 17:18:39 +0100

    add act as token authenticable to model user

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::MoviesController < Api::V1::BaseController
+  acts_as_token_authentication_handler_for User, except: [ :index, :show ]
   before_action :set_movie, only: [ :show ]
 
   def index
@@ -8,8 +9,28 @@ class Api::V1::MoviesController < Api::V1::BaseController
   def show
   end
 
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.user = current_user
+    authorize @movie
+    if @movie.save
+      render :show, status: :created
+    else
+      render_error
+    end
+  end
+
+  def destroy
+
+  end
+
   def set_movie
     @movie = Movie.find(params[:id])
-    authorize @movie  # For Pundit
+    authorize @movie
+  end
+
+  def render_error
+    render json: { errors: @movie.errors.full_messages },
+      status: :unprocessable_entity
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
-  acts_as_token_authenticatable
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
     has_many :reviews
     has_many :movies
+
+  acts_as_token_authenticatable
 end
